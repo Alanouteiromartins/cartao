e2338efcca737617d1395bf57cd29ac879edc869
Alan <alanoutmartins@gmail.com>
2025-04-11 13:12:17 -0300

    update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/usuarios' 
+  apiUrl: 'https://apicartao-im9y.onrender.com/usuarios' 
 };
 
-
-//https://apicartao-im9y.onrender.com/usuarios
